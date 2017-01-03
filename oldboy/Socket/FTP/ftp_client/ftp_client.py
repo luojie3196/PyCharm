4b4d082633487bc3157ed58911ee77369f7983eb
luojie <luojie3196@163.com>
2017-01-03 16:19:13 +0800

    FTP develop stage 2

--- oldboy/Socket/FTP/ftp_client/ftp_client.py
+++ oldboy/Socket/FTP/ftp_client/ftp_client.py
@@ -21,6 +21,7 @@ class FtpClient(object):
         get filename
         put filename
         '''
+        print(msg)
 
     def connect(self, ip, port):
         self.client.connect((ip, port))
@@ -45,7 +46,7 @@ class FtpClient(object):
             filename = cmd_split[1]
             if os.path.isfile(filename):
                 pass
-                # TODO: 文件大小和文件名组合成字典用json.dumps() send给server
+                # 文件大小和文件名组合成字典用json.dumps() send给server
                 filesize = os.stat(filename).st_size
                 msg_dic = {
                     "action": "put",
@@ -55,22 +56,77 @@ class FtpClient(object):
                 }
                 self.client.send(json.dumps(msg_dic).encode("utf-8"))
                 print("cmd_put-send ", msg_dic)
-                # TODO: 防止粘包，等服务器确认,可用于确认剩余空间够不够
+                # 防止粘包，等服务器确认,用于确认剩余空间够不够
                 server_response = self.client.recv(1024)
-                # TODO: .....
-                f = open(filename, "rb")
-                for line in f:
-                    self.client.send(line)
+                status_dic = json.loads(server_response.decode())
+                space_status = status_dic["space_status"]
+                if space_status:
+                    m = hashlib.md5()
+                    f = open(filename, "rb")
+                    for line in f:
+                        m.update(line)
+                        self.client.send(line)
+                    else:
+                        f.close()
+                        md5_str = self.client.recv(1024)
+                        recv_md5 = md5_str.decode()
+                        file_md5 = m.hexdigest()
+                        if file_md5 == recv_md5:
+                            print("%s file put success..." % filename)
+                            # print("md5: %s" % m.hexdigest())
+                            self.client.send(b"OK")
+                        else:
+                            print("MD5 check failed...")
+                            print("%s file put failed..." % filename)
+                            self.client.send(b"NOK")
                 else:
-                    print("%s file upload success..." % filename)
-                    f.close()
+                    print("Server not enough space")
             else:
                 print(filename, "is not exist")
-                pass
-        pass
 
-    def cmd_get(self):
-        pass
+    def cmd_get(self, *args):
+        cmd_split = args[0].split()
+        if len(cmd_split) > 1:
+            filename = cmd_split[1]
+            msg_dic = {
+                "action": "get",
+                "filename": filename,
+            }
+            self.client.send(json.dumps(msg_dic).encode("utf-8"))
+            print("cmd_get-send ", msg_dic)
+            server_response = self.client.recv(1024)
+            msg_dic = json.loads(server_response.decode())
+            file_status = msg_dic["file_status"]
+            self.client.send(b"Ready to receive...")
+            if file_status:
+                file_size = msg_dic["file_size"]
+                recv_size = 0
+                m = hashlib.md5()
+                f = open(filename, "wb")
+                while recv_size < file_size:
+                    if file_size - recv_size > 1024:
+                        size = 1024
+                    else:
+                        size = file_size - recv_size
+                        print("Last recv size: ", size)
+                    data = self.client.recv(size)
+                    f.write(data)
+                    m.update(data)
+                    recv_size += len(data)
+                else:
+                    f.close()
+                    file_md5 = m.hexdigest()
+                    self.client.send(file_md5.encode("utf-8"))
+                    md5_check_status = self.client.recv(1024).decode()
+                    print("md5_check_status: ", md5_check_status)
+                    if md5_check_status == "OK":
+                        print("%s file get success..." % filename)
+                        # print("md5: %s" % m.hexdigest())
+                    else:
+                        print("%s file get failed..." % filename)
+                        os.remove(filename)
+            else:
+                print("Server didn't exist [%s] file" % filename)
 
 ftp = FtpClient()
 ftp.connect("localhost", 9999)
--- oldboy/Socket/FTP/ftp_server/core/main.py
+++ oldboy/Socket/FTP/ftp_server/core/main.py
@@ -4,26 +4,107 @@
 import socketserver
 import json
 import os
+import subprocess
+import hashlib
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
+    def get_space(self, path):
+        ret = subprocess.Popen("df %s | tail -1" % path, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+        ret_str = ret.stdout.read()
+        left_space = ret_str.decode().split()[3]
+        return int(left_space) * 1024
+
+    def get(self, *args):
+        cmd_dic = args[0]
+        filename = cmd_dic["filename"]
+        if os.path.isfile(filename):
+            file_status = True
+            file_size = os.stat(filename).st_size
+        else:
+            file_status = False
+            file_size = None
+        msg_dic = {
+            "file_status": file_status,
+            "file_size": file_size
+        }
+        self.request.send(json.dumps(msg_dic).encode("utf-8"))
+        self.request.recv(1024)
+        if file_status:
+            m = hashlib.md5()
+            f = open(filename, "rb")
+            for line in f:
+                self.request.send(line)
+                m.update(line)
+            else:
+                f.close()
+                md5_str = self.request.recv(1024)
+                recv_md5 = md5_str.decode()
+                file_md5 = m.hexdigest()
+                if file_md5 == recv_md5:
+                    print("%s file get success..." % filename)
+                    # print("md5: %s" % m.hexdigest())
+                    self.request.send(b"OK")
+                else:
+                    print("MD5 check failed...")
+                    print("%s file get failed..." % filename)
+                    self.request.send(b"NOK")
+                # print("[%s] file send success..." % filename)
+            f.close()
+        else:
+            print("[%s] file not exist" % filename)
+
     def put(self, *args):
         cmd_dic = args[0]
         filename = cmd_dic["filename"]
-        filesize = cmd_dic["size"]
-        if os.path.isfile(filename):
-            f = open(filename + ".new", "wb")
+        file_size = cmd_dic["size"]
+        overridden = cmd_dic["overridden"]
+        # TODO: 如何动态传入path值
+        left_space = self.get_space(".")
+        print("left_space: ", left_space)
+        print("file size: ", file_size)
+        if left_space < file_size:
+            space_status = False
         else:
-            f = open(filename, "wb")
-        self.request.send(b"200 OK")
-        received_size = 0
-        while received_size < filesize:
-            data = self.request.recv(1024)
-            f.write(data)
-            received_size += len(data)
+            space_status = True
+        print("space_status: ", space_status)
+        msg_dic = {
+            "space_status": space_status
+        }
+        self.request.send(json.dumps(msg_dic).encode("utf-8"))
+        if space_status:
+            if os.path.isfile(filename) and not overridden:
+                f = open(filename + ".new", "wb")
+            else:
+                f = open(filename, "wb")
+            recv_size = 0
+            m = hashlib.md5()
+            while recv_size < file_size:
+                if file_size - recv_size > 1024:
+                    size = 1024
+                else:
+                    size = file_size - recv_size
+                    print("Last recv size: ", size)
+                data = self.request.recv(size)
+                f.write(data)
+                m.update(data)
+                recv_size += len(data)
+            else:
+                f.close()
+                file_md5 = m.hexdigest()
+                self.request.send(file_md5.encode("utf-8"))
+                md5_check_status = self.request.recv(1024).decode()
+                print("md5_check_status: ", md5_check_status)
+                if md5_check_status == "OK":
+                    print("%s file put success..." % filename)
+                    # print("md5: %s" % m.hexdigest())
+                else:
+                    print("%s file put failed..." % filename)
+                    os.remove(filename)
+
         else:
-            print("File [%s] has uploaded ..." % filename)
+            print("No space left on device")
 
     def handle(self):
         while True:
